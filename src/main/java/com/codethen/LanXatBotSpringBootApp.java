f9de6d12e30670feb3c9859ed3c1c60c8762cb16
Ferran Maylinch <ferran@yandex-team.ru>
2023-02-26 14:45:27 +0100

    Avoid Spring Boot automatic connection to mongodb

--- src/main/java/com/codethen/LanXatBotSpringBootApp.java
+++ src/main/java/com/codethen/LanXatBotSpringBootApp.java
@@ -2,8 +2,13 @@ package com.codethen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoReactiveAutoConfiguration;
 
-@SpringBootApplication
+/**
+ * Exclude is to prevent SpringBoot from trying to connect to localhost.
+ * https://stackoverflow.com/questions/68121079/spring-boot-mongo#comment133330046_68121079
+ */
+@SpringBootApplication(exclude=MongoReactiveAutoConfiguration.class)
 public class LanXatBotSpringBootApp {
 
     public static void main(String... args) {
